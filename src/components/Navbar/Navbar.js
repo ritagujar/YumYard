9d33b29a7fb26a3e2086496a2be78d3c04d65231
ritagujar <ritagujar14@gmail.com>
2024-03-21 15:08:42 +0530

    Initial Commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Heading, NavbarSection } from "./styles";
+
+const Navbar = () => {
+  return (
+    <NavbarSection>
+      <Heading>YumYard</Heading>
+    </NavbarSection>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const NavbarSection = styled.div`
+  background-color: rebeccapurple;
+`;
+
+export const Heading = styled.h1`
+  font-size: 2rem;
+`;
