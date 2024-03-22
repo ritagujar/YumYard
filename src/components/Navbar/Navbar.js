25839a8441dfe1471ffc169d43f9fa8fd93b93aa
ritagujar <ritagujar14@gmail.com>
2024-03-22 20:36:37 +0530

    Created navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { Heading, NavbarSection } from "./styles";
+import {
+  Heading,
+  HeadingSection,
+  Logo,
+  MenuSection,
+  NavbarSection,
+} from "./styles";
+import logo from "../../images/hat_7270989.png";
+import logo2 from "../../images/wooden-tableware.png";
 
 const Navbar = () => {
   return (
     <NavbarSection>
-      <Heading>YumYard</Heading>
+      <HeadingSection>
+        <img src={logo} alt="Logo" />
+        <h2>YumYard</h2>
+      </HeadingSection>
+      <MenuSection>
+        <button>Sign in</button>
+      </MenuSection>
     </NavbarSection>
   );
 };
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,9 +1,42 @@
 import styled from "styled-components";
 
 export const NavbarSection = styled.div`
-  background-color: rebeccapurple;
+  width: 100%;
+  height: 5rem;
+  /* background-color: aqua; */
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-export const Heading = styled.h1`
-  font-size: 2rem;
+export const HeadingSection = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  cursor: pointer;
+
+  h2 {
+    font-size: 1.4rem;
+    font-weight: 700;
+  }
+  img {
+    width: 1.7rem;
+    height: 1.7rem;
+  }
+`;
+
+export const MenuSection = styled.div`
+  button {
+    width: 5.5rem;
+    height: 2.2rem;
+    border: none;
+    font-size: 0.94rem;
+    font-family: "Poppins", sans-serif;
+    font-weight: 600;
+    background-color: #262626;
+    color: #ffffff;
+    border-radius: 10px;
+    letter-spacing: 0.7px;
+    cursor: pointer;
+  }
 `;
