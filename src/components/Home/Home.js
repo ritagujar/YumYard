0500b9af81a39232d45219a37e7e5318ddc04508
ritagujar <ritagujar14@gmail.com>
2024-03-24 21:25:28 +0530

    Designed the home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,9 +13,16 @@ const Home = () => {
       <ContainerSection>
         <LeftSection>
           <h1>
-            Delicious <span>Dining Savor</span>he Flavor Delivered to Your{" "}
+            Delicious <span>Dining Savor </span>the Flavor Delivered to Your{" "}
             <span>Door Step</span>
           </h1>
+          <p>
+            Discover a world of culinary delight right at your fingertips with
+            Delicious Dining, where you can indulge in an array of mouthwatering
+            dishes delivered directly to your doorstep. Experience gourmet
+            cuisine from renowned restaurants, thoughtfully selected and
+            conveniently brought to you.
+          </p>
         </LeftSection>
         <RightSection>
           <img src={homeBanner} alt="Banner" />
--- src/components/Home/style.js
+++ src/components/Home/style.js
@@ -1,18 +1,46 @@
 import styled from "styled-components";
 
 export const HomeSection = styled.div`
-  padding: 2rem 4rem 0 4rem;
+  padding: 4rem 4rem 0 4rem;
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const ContainerSection = styled.div`
   display: flex;
 `;
 
-export const LeftSection = styled.div``;
+export const LeftSection = styled.div`
+  margin-right: 4rem;
+  display: flex;
+  flex-direction: column;
+  max-width: 70%;
+  gap: 2rem;
+  h1 {
+    line-height: 4rem;
+    font-size: 3rem;
+  }
+
+  span {
+    color: #f97316;
+  }
+
+  p {
+    font-size: 1rem;
+    font-weight: 400;
+    letter-spacing: 0.7px;
+    line-height: 2rem;
+  }
+`;
 
 export const RightSection = styled.div`
+  width: 50%;
+  align-items: center;
+  text-align: center;
   img {
-    max-width: 100%;
-    height: 20rem;
+    width: 15rem;
+    border-radius: 5px;
+    object-fit: cover;
+    height: 25rem;
   }
 `;
